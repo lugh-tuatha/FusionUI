4becbb2c214020a9d53d0a444a07ea4361180c49
Ace <aceonsteam001@gmail.com>
2023-05-28 17:14:31 +0800

    fix: spacing, add: showcase box components

---
+++ src/components/showcase-thumb/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './showcase-thumb.css'
+
+function ShowcaseThumb() {
+  return (
+    <div className='showcase-thumb'>
+      
+    </div>
+  )
+}
+
+export default ShowcaseThumb
